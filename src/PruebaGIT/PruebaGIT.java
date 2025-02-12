80fa98f7ac0375b5d06d2d15ec38f5eb88eaa477
Danielppc06 <danielppc06@gmail.com>
2025-02-12 16:47:04 +0100

    commit en rama

--- src/PruebaGIT/PruebaGIT.java
+++ src/PruebaGIT/PruebaGIT.java
@@ -5,7 +5,7 @@ public class PruebaGIT {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HOLA MUNDO");
-
+		System.out.println("Hola mundo de nuevo");
 	}
 
 }
